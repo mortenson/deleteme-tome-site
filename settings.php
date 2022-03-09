74940e69021c10d9dd5e6e9aa595744e07d60ccd
Samuel Mortenson <mortensonsam@gmail.com>
2022-03-09 14:26:24 +0000

    Init

--- settings.php
+++ settings.php
@@ -297,7 +297,7 @@ $config_directories = array();
  *   $settings['hash_salt'] = file_get_contents('/home/example/salt.txt');
  * @endcode
  */
-$settings['hash_salt'] = '';
+$settings['hash_salt'] = 'Npc4HPT6UwE0VLAiRVK71nxu_9ib8E0iSzKXf0OeQalV8gyjDVghSd5x-QHCRo3BFjHzlBX7SQ';
 
 /**
  * Deployment identifier.
@@ -805,3 +805,9 @@ $settings['config_sync_directory'] = '../config';
 //   $config['system.performance']['css']['preprocess'] = FALSE;
 //   $config['system.performance']['js']['preprocess'] = FALSE;
 // }
+$databases['default']['default'] = array (
+  'database' => 'sites/default/files/.ht.sqlite',
+  'prefix' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\sqlite',
+  'driver' => 'sqlite',
+);
